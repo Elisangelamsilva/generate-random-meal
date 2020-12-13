0b3d199c7e63458c992d079ceb297acd522ce879
Elisangela <elisangela00silva@gmail.com>
2020-12-13 14:22:30 -0300

    adição da gem rest-client
    Signed-off-by: Elisangela <elisangela00silva@gmail.com>

--- app/controllers/initial_controller.rb
+++ app/controllers/initial_controller.rb
@@ -1,4 +1,8 @@
 class InitialController < ApplicationController
   def index
+    busca = RestClient.get 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+    @meal_resul = busca.body
+
   end
 end
